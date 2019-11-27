7a6cb1c64b5f48f8df8c889c01346f7bbb6384a6
bartamihai <mihai.barta@evozon.com>
2019-11-27 15:09:46 +0200

    Commit made using teleportHQ

---
+++ src/components/component.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './component.module.css'
+
+const AppComponent = (props) => {
+  return <div className={styles.component}></div>
+}
+
+export default AppComponent
---
+++ src/views/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+import styles from './page.module.css'
+
+const Page = (props) => {
+  return (
+    <div className={styles.page}>
+      <Helmet>
+        <title>u8FqCx6Zcs</title>
+      </Helmet>
+    </div>
+  )
+}
+
+export default Page
